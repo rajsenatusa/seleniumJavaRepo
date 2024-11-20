764abbdc2c9737a4398f08fcb98c7120bcae8111
srajendran <srajendran@AI3FQ0WS14.AIIG.local>
2024-11-20 15:06:29 -0500

    commit for run.bat changes

--- InsuranceNow_v1.0/run.bat
+++ InsuranceNow_v1.0/run.bat
@@ -1,2 +1,5 @@
-cd C:\Users\SRajendran\git\seleniumJavaRepo\InsuranceNow_v1.0\
-mvn test
+@echo off
+echo Running Maven clean and test...
+
+cd C:\Users\srajendran\git\seleniumJavaRepo\InsuranceNow_v1.0
+mvn clean && mvn test
