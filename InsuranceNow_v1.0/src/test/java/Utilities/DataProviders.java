c9a788220013d61d7c234b4e0c6cf927270336eb
srajendran <srajendran@AI3FQ0WS14.AIIG.local>
2024-11-21 16:42:04 -0500

    working on deactivate user

--- InsuranceNow_v1.0/src/test/java/Utilities/DataProviders.java
+++ InsuranceNow_v1.0/src/test/java/Utilities/DataProviders.java
@@ -9,7 +9,7 @@ public class DataProviders {
 	//DataProvider 1
 	
 	@DataProvider(name="providerData")
-	public String [][] getData() throws IOException
+	public String [][] getProviderData() throws IOException
 	{
 		String path=".\\testData\\providerData.xlsx";//taking xl file from testData
 		
@@ -56,7 +56,31 @@ public class DataProviders {
 				
 	}
 	
+	
 	//DataProvider 3
+	
+	@DataProvider(name="deactivateUserData")
+	public String [][] getDeactivateUserData() throws IOException
+	{
+		String path=".\\testData\\deactivateUserData.xlsx";//taking xl file from testData
+		
+		ExcelUtility xlutil=new ExcelUtility(path);//creating an object for XLUtility
+		
+		int totalrows=xlutil.getRowCount("Sheet1");	
+		int totalcols=xlutil.getCellCount("Sheet1",1);
+				
+		String providerdata[][]=new String[totalrows][totalcols];//created for two dimension array which can store the data user and password
+		
+		for(int i=1;i<=totalrows;i++)  //1   //read the data from xl storing in two deminsional array
+		{		
+			for(int j=0;j<totalcols;j++)  //0    i is rows j is col
+			{
+				providerdata[i-1][j]= xlutil.getCellData("Sheet1",i, j);  //1,0
+			}
+		}
+	return providerdata;//returning two dimension array
+				
+	}
 	
 	//DataProvider 4
 }
--- InsuranceNow_v1.0/src/test/java/pageObjects/InHomePage.java
+++ InsuranceNow_v1.0/src/test/java/pageObjects/InHomePage.java
@@ -40,7 +40,9 @@ public class InHomePage extends basePage {
 	public void loginInsuranceNow(String username, String password) {
 		// This action method login's to insurance now.
 		txtUsername.click();
+		txtUsername.clear();
 		txtUsername.sendKeys(username);
+		txtPassword.clear();
 		txtPassword.sendKeys(password);
 		btnSignIn.click();
 	}
