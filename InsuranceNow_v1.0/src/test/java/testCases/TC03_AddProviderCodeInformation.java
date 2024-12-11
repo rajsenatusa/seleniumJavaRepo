19da41d40a08aa5c469c593c275e7e32b6060bfd
srajendran <srajendran@AI3FQ0WS14.AIIG.local>
2024-12-11 16:33:31 -0500

    modified add provider code

--- InsuranceNow_v1.0/src/test/java/testCases/TC03_AddProviderCodeInformation.java
+++ InsuranceNow_v1.0/src/test/java/testCases/TC03_AddProviderCodeInformation.java
@@ -1,8 +1,6 @@
 package testCases;
-
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.time.Duration;
@@ -27,198 +25,179 @@ import testBase.testBaseClass;
 
 public class TC03_AddProviderCodeInformation extends testBaseClass {
 	
+    final String RESULTS_FOLDER = "DataDrivenResults";
+    final String RESULTS_FILE = RESULTS_FOLDER + "/TestResults.xlsx";
+
 
     @Test(dataProvider = "providerData", dataProviderClass = DataProviders.class, groups = {"datadriven"})
-    public void verify_copy_provider(String username, String password, String firstname, String lastname, String phonenumber,
+    public void verify_addProviderCode(String username, String password, String firstname, String lastname, String phonenumber,
                                      String licensenumber, String licensexp, String emailaddress, String manager, String roleofuser) {
 
-        logger.info(" **Add Provider Code Test Started! **");      
-        
+        logger.info(" **Add Provider Code Test Started! **");
+
         try {
-        	
-            // Logging input data for visibility
-            logger.info("User Data: " + String.format("Username: %s, Password: %s, FirstName: %s, LastName: %s", username, password, firstname, lastname));
+            validateInputData(username, password, firstname, lastname, phonenumber, licensenumber, licensexp, emailaddress, manager, roleofuser);
 
-            // Check for null data in provider fields
-            if (username == null || password == null || firstname == null || lastname == null ||
-                phonenumber == null || licensenumber == null || licensexp == null ||
-                emailaddress == null || manager == null || roleofuser == null) {
-
-                logger.error("Missing required data. Skipping test.");
-                throw new SkipException("Skipping test due to missing required data.");
-            }
-
-            // Home Page - Log in
+            // Log in to the application
             InHomePage hp = new InHomePage(getDriver());
-            logger.info("Entering login credentials for user: " + username );
+            logger.info("Logging in with username: " + username);
             hp.loginInsuranceNow(username, password);
-
 
             // Navigate to Claims Maintenance Page
             logger.info("Navigating to Claims Maintenance page...");
             InClaimsMaintenancePage cmp = new InClaimsMaintenancePage(getDriver());
             cmp.navigateToNewProviderPage();
-            
+
             // Generate provider number
-            
-            String ProviderCode = generateProviderNumber("CA0", 4);
-            logger.info("Generated Provider Code: " + ProviderCode);
-            cmp.setProviderNumber(ProviderCode);
-            
-            cmp.selectProviderType("Adjuster");
-            String currentDate = getCurrentDate("MM/dd/yyyy");
-            cmp.setProviderStatusDate(currentDate);
+            String providerCode = generateProviderNumber("CA0", 4);
+            logger.info("Generated Provider Code: " + providerCode);
+
+            fillProviderDetails(cmp, providerCode, firstname, lastname, phonenumber);
+            // Validate that the provider code is saved correctly
+            validateProviderCode(cmp, providerCode);
+            // Add new territory location
+            addTerritoryLocation(cmp);
+            // Handle license details
+            handleLicenseDetails(cmp, licensenumber, licensexp);
+            // Write results to Excel
                         
-            cmp.selectStatus("Active");
-            cmp.selectBusinessType("Individual");   
-            cmp.selectAllowCombinedPayment("Yes");
-            String fullName = generateFullName(firstname, lastname);
-            cmp.setBusinessName(fullName);
-            cmp.setName(fullName);    
-            cmp.setDBAName(fullName);
-            cmp.setStreeAddress("5424 Bay Center Dr", "Tampa", "FL", "33609");
-            cmp.setPrimaryPhoneNumber("Mobile", phonenumber);
-            cmp.setSecondaryPhoneNumber("Mobile", phonenumber);
-            cmp.clickCopyBillingAddress();
-            cmp.select1099Required("No");
-            cmp.selectTaxIDType("SSN");
-            //cmp.setTaxID("123456798");
-            cmp.selectPaymentPreference("Check");
-            cmp.clickSaveBtn();
+            writeResultsToExcel(username, password, firstname, lastname, phonenumber, licensenumber, licensexp, emailaddress, manager, roleofuser, providerCode);
 
-            // Wait for save confirmation
-            WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(50)); // Adjust timeout as needed
-            wait.until(ExpectedConditions.textToBePresentInElement(cmp.getSaveConfirmationElement(), ProviderCode));
-
-            // Validate that the correct provider code was saved
-            String capturedProviderCode = cmp.getProviderCode();
-            Assert.assertEquals(capturedProviderCode, ProviderCode, "Provider Code mismatch after saving.");
-            logger.info("Provider code successfully added and verified: " + capturedProviderCode);      
-            
-            
-            //add new NewTerritory Location
-            
-            cmp.setNewTerrirotyLocationDetails("FL", "State", "Adding Territory Location");
-            
-            if (licensexp.isEmpty()) {
-            	
-                // Case 1: License expiration date is blank, set it to "12/31/9999"
-            
-                cmp.setLicenseDetails("FL", "PCAgent", licensenumber, "12/31/9999", "License is valid");
-                
-            } else {
-            	
-                // Parse the license expiration date
-            	String liceExpirationDate = convertDateToExpectedFormat(licensexp);
-                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
-                LocalDate expirationDate = LocalDate.parse(liceExpirationDate, formatter);
-                LocalDate TodayDate = LocalDate.now();
-
-                if (expirationDate.isBefore(TodayDate)) {
-                    // Case 2: License expiration date is in the past
-                    cmp.setLicenseDetails("FL", "PCAgent", licensenumber, liceExpirationDate, "Not Licensed");
-                } else {
-                    // Case 3: License expiration date is in the future or today
-                    cmp.setLicenseDetails("FL", "PCAgent", licensenumber, liceExpirationDate, "License is valid");
-                }
-            }
-                      
-          
-            // Write the generated provider code to the Excel sheet     
-            logger.info("Provider Code details are started written in to excel sheet...");  
-            writeResultsToExcel( username,  password,  firstname,  lastname,  phonenumber,
-            		licensenumber,  licensexp,  emailaddress,  manager,  roleofuser, capturedProviderCode);
-        
-            logger.info("Written provider code successfully in the Excel Sheet: " + capturedProviderCode);
-            
-         // Log out
+            // Sign out
             cmp.SignOutInsuranceNow();
-            logger.info("Provider code adding test completed successfully and user signed out.");  
-            
-    
-                    
-        } catch (SkipException e) {
-            logger.warn("Test Skipped: " + e.getMessage());
-            throw e;  // Re-throw to ensure it's logged as skipped in reports
+            logger.info("Provider code test completed successfully.");
 
+        } catch (SkipException e) {
+            logger.warn("Test skipped: " + e.getMessage());
+            throw e;
         } catch (Exception e) {
             logger.error("Exception occurred: " + e.getMessage(), e);
             Assert.fail("Test failed due to an exception: " + e.getMessage());
-
         } finally {
-            logger.info(" **Add New Provider Number Test Completed! **");
+            logger.info(" **Add Provider Code Test Completed! **");
         }
     }
-    
-    
 
-	private void writeResultsToExcel(String username, String password, String firstname, String lastname,
-			String phonenumber, String licensenumber, String licensexp, String emailaddress, String manager,
-			String roleofuser, String capturedProviderCode) throws FileNotFoundException, IOException {
+    private void validateInputData(String... fields) {
+        for (String field : fields) {
+            if (field == null || field.isEmpty()) {
+                logger.error("Missing required input data. Skipping test.");
+                throw new SkipException("Skipping test due to missing required data.");
+            }
+        }
+    }
 
-			final String RESULTS_FOLDER = "DataDrivenResults";
-			final String RESULTS_FILE = RESULTS_FOLDER + "/TestResults.xlsx";
-			
-			// Ensure the results folder exists
-	        File resultsDir = new File(RESULTS_FOLDER);
-	        if (!resultsDir.exists()) {
-	            resultsDir.mkdirs();
-	        }
+    private void fillProviderDetails(InClaimsMaintenancePage cmp, String providerCode, String firstname, String lastname, String phonenumber) {
+        cmp.setProviderNumber(providerCode);
+        cmp.selectProviderType("Adjuster");
+        cmp.setProviderStatusDate(getCurrentDate("MM/dd/yyyy"));
+        cmp.selectStatus("Active");
+        cmp.selectBusinessType("Individual");
+        cmp.selectAllowCombinedPayment("Yes");
+        String fullName = generateFullName(firstname, lastname);
+        cmp.setBusinessName(fullName);
+        cmp.setName(fullName);
+        cmp.setDBAName(fullName);
+        cmp.setStreeAddress("5424 Bay Center Dr", "Tampa", "FL", "33609");
+        cmp.setPrimaryPhoneNumber("Mobile", phonenumber);
+        cmp.setSecondaryPhoneNumber("Mobile", phonenumber);
+        cmp.clickCopyBillingAddress();
+        cmp.select1099Required("No");
+        cmp.selectTaxIDType("SSN");
+        cmp.selectPaymentPreference("Check");
+        cmp.clickSaveBtn();
+    }
 
-	        Workbook workbook;
-	        Sheet sheet;
+    private void validateProviderCode(InClaimsMaintenancePage cmp, String providerCode) {
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(50));
+        wait.until(ExpectedConditions.textToBePresentInElement(cmp.getSaveConfirmationElement(), providerCode));
 
-	        // Check if the file exists to either create a new workbook or update the existing one
-	        File file = new File(RESULTS_FILE);
-	        
-	        // Check if the file exists and has a non-zero length
-	        if (file.exists() && file.length() > 0) {
-	            // File exists and is not empty, open it
-	            try (FileInputStream fis = new FileInputStream(file)) {
-	                workbook = WorkbookFactory.create(fis);
-	                sheet = workbook.getSheetAt(0);
-	            }
-	            
-	        } else {
-	            workbook = new XSSFWorkbook();
-	            sheet = workbook.createSheet("Test Results");
-	            // Create the header row
-	            Row headerRow = sheet.createRow(0);
-	            headerRow.createCell(0).setCellValue("Username");
-	            headerRow.createCell(1).setCellValue("Password");
-	            headerRow.createCell(2).setCellValue("First Name");
-	            headerRow.createCell(3).setCellValue("Last Name");
-	            headerRow.createCell(4).setCellValue("Phone Number");
-	            headerRow.createCell(5).setCellValue("License #");
-	            headerRow.createCell(6).setCellValue("License Expiration Date");
-	            headerRow.createCell(7).setCellValue("Email Address");
-	            headerRow.createCell(8).setCellValue("Manager");
-	            headerRow.createCell(9).setCellValue("Role");
-	            headerRow.createCell(10).setCellValue("Producer Code");
+        String capturedProviderCode = cmp.getProviderCode();
+        Assert.assertEquals(capturedProviderCode, providerCode, "Provider Code mismatch after saving.");
+        logger.info("Provider code successfully added and verified: " + capturedProviderCode);
+    }
 
-	        }
+    private void addTerritoryLocation(InClaimsMaintenancePage cmp) {
+        cmp.setNewTerrirotyLocationDetails("FL", "State", "Adding Territory Location");
+    }
 
-		        // Write data to the next available row
-		        int rowCount = sheet.getLastRowNum();
-		        Row row = sheet.createRow(rowCount + 1);	
-		        row.createCell(0).setCellValue(username);
-		        row.createCell(1).setCellValue(password);
-		        row.createCell(2).setCellValue(firstname);
-		        row.createCell(3).setCellValue(lastname);
-		        row.createCell(4).setCellValue(phonenumber);
-		        row.createCell(5).setCellValue(licensenumber);
-		        row.createCell(6).setCellValue(licensexp);
-		        row.createCell(7).setCellValue(emailaddress);
-		        row.createCell(8).setCellValue(manager);
-		        row.createCell(9).setCellValue(roleofuser);
-		        row.createCell(10).setCellValue(capturedProviderCode);
-	       
+    private void handleLicenseDetails(InClaimsMaintenancePage cmp, String licensenumber, String licensexp) {
+        if (licensexp == null || licensexp.isEmpty()) {
+            cmp.setLicenseDetails("FL", "PCAgent", licensenumber, "12/31/9999", "License is valid");
+        } else {
+            String formattedLicenseExp = convertDateToExpectedFormat(licensexp);
+            LocalDate expirationDate = LocalDate.parse(formattedLicenseExp, DateTimeFormatter.ofPattern("MM/dd/yyyy"));
+            LocalDate today = LocalDate.now();
 
-	        // Write the results back to the file
-	        try (FileOutputStream fos = new FileOutputStream(RESULTS_FILE)) {
-	            workbook.write(fos);
-	        }
-	        workbook.close();
-	    }
+            if (expirationDate.isBefore(today)) {
+                cmp.setLicenseDetails("FL", "PCAgent", licensenumber, formattedLicenseExp, "Not Licensed");
+            } else {
+                cmp.setLicenseDetails("FL", "PCAgent", licensenumber, formattedLicenseExp, "License is valid");
+            }
+        }
+    }
+
+    private void writeResultsToExcel(String username, String password, String firstname, String lastname,
+                                     String phonenumber, String licensenumber, String licensexp, String emailaddress,
+                                     String manager, String roleofuser, String providerCode) throws IOException {
+
+        // Ensure results folder exists
+        File resultsDir = new File(RESULTS_FOLDER);
+        if (!resultsDir.exists()) resultsDir.mkdirs();
+       
+        // Set up workbook and sheet
+        Workbook workbook = getWorkbook();
+        Sheet sheet = workbook.getSheetAt(0);
+        
+        // Write test data row to Excel
+        writeDataRow(sheet, username,  password,  firstname,  lastname,
+                 phonenumber,  licensenumber,  licensexp,  emailaddress,
+                 manager,  roleofuser,  providerCode);
+
+        // Write changes to Excel file
+        try (FileOutputStream fos = new FileOutputStream(RESULTS_FILE)) {
+            workbook.write(fos);
+        }
+        workbook.close();
+    } 
+  
+
+	private Workbook getWorkbook() throws IOException {
+        File file = new File(RESULTS_FILE);
+        Workbook workbook;
+
+        if (file.exists() && file.length() > 0) {
+            try (FileInputStream fis = new FileInputStream(file)) {
+                workbook = WorkbookFactory.create(fis);
+            }
+        } else {
+            workbook = new XSSFWorkbook();
+            Sheet sheet = workbook.createSheet("Test Results");
+            createHeaderRow(sheet);
+        }
+        return workbook;
+	}
+
+	private void createHeaderRow(Sheet sheet) {
+		Row headerRow = sheet.createRow(0);
+        String[] headers = {"Username", "Password", "First Name", "Last Name", "Phone Number", "License #",
+                            "License Expiration Date", "Email Address", "Manager", "Role", "Provider Code"};
+        for (int i = 0; i < headers.length; i++) {
+            headerRow.createCell(i).setCellValue(headers[i]);
+        }
+	}
 	
+	private void writeDataRow(Sheet sheet, String username, String password, String firstname, String lastname,
+			String phonenumber, String licensenumber, String licensexp, String emailaddress, String manager,
+			String roleofuser, String providerCode) {
+		 int rowCount = sheet.getLastRowNum();
+	        Row row = sheet.createRow(rowCount + 1);
+
+	        String[] values = {username, password, firstname, lastname, phonenumber, licensenumber, licensexp,
+	                           emailaddress, manager, roleofuser, providerCode};
+
+	        for (int i = 0; i < values.length; i++) {
+	            row.createCell(i).setCellValue(values[i]);
+	        }
+	    }
 }
