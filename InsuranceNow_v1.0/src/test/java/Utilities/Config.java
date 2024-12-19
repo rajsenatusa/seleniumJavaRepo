2035212a9cc173b41957391f99df764bb09731cc
srajendran <srajendran@AI3FQ0WS14.AIIG.local>
2024-12-19 16:31:03 -0500

    password reset script udpated

--- InsuranceNow_v1.0/src/test/java/Utilities/Config.java
+++ InsuranceNow_v1.0/src/test/java/Utilities/Config.java
@@ -6,4 +6,8 @@ public class Config {
     public static final String LOGIN_DATA_PATH = ".\\testData\\loginData.xlsx";
     public static final String DEACTIVATE_USER_DATA_PATH = ".\\testData\\deactivateUserData.xlsx";
     public static final String RESET_PASSWORD_USER_DATA_PATH = ".\\testData\\resetPasswordUserData.xlsx";
+    
+	public static final String STATUS_ACTIVE = "Active";
+	public static final String STATUS_TERMINATED = "Terminated";
+	public static final String DATE_FORMAT = "MM/dd/yyyy";
 }
